f5e7fd78752bebe8575a4d16d9ab203012893f9a
Ismail Arafa <ismailarafa18@gmail.com>
2018-02-22 03:18:46 +0200

    BEM-ify css

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-var i;
-var view = {
-  hideText: function () {
-    var proj = document.getElementById('blog');
-    var projChild = proj.children;
-    for (i = 0; i < projChild.length; i += 1) {
-      if (projChild[i].tagName !== 'H2' && projChild[i].tagName !== 'H3') {
-        projChild[i].style.display = 'none';
+const view = {
+  hideText() {
+    const proj = document.getElementById("blog");
+    const projChild = proj.children;
+    for (let i = 0; i < projChild.length; i += 1) {
+      if (projChild[i].tagName !== "H2" && projChild[i].tagName !== "H3") {
+        projChild[i].style.display = "none";
       }
     }
   },
-  toggleTxt: function (txt) {
-    var text = document.getElementById(txt);
-    if (text.style.display === 'none') {
-      text.style.display = 'initial';
+  toggleTxt(txt) {
+    const text = document.getElementById(txt);
+    if (text.style.display === "none") {
+      text.style.display = "initial";
     } else {
-      text.style.display = 'none';
+      text.style.display = "none";
     }
   }
 };
 
-var handlers = {
-  setUpEvents: function () {
-    var proj = document.getElementById('blog');
-    proj.addEventListener('click', function (event) {
-      var elementClicked = event.target;
-      if (elementClicked.id === 'intro') {
-        view.toggleTxt('intro-txt');
-      } else if (elementClicked.id === 'src') {
-        view.toggleTxt('src-txt');
-      } else if (elementClicked.id === 'newdev') {
-        view.toggleTxt('dev-txt');
-      } else if (elementClicked.id === 'dsgn') {
-        view.toggleTxt('dsgn-txt');
-      } else if (elementClicked.id === 'js') {
-        view.toggleTxt('js-txt');
+const handlers = {
+  setUpEvents() {
+    const proj = document.getElementById("blog");
+    proj.addEventListener("click", event => {
+      const elementClicked = event.target;
+      if (elementClicked.id === "intro") {
+        view.toggleTxt("intro-txt");
+      } else if (elementClicked.id === "src") {
+        view.toggleTxt("src-txt");
+      } else if (elementClicked.id === "newdev") {
+        view.toggleTxt("dev-txt");
+      } else if (elementClicked.id === "dsgn") {
+        view.toggleTxt("dsgn-txt");
+      } else if (elementClicked.id === "js") {
+        view.toggleTxt("js-txt");
       }
     });
   }

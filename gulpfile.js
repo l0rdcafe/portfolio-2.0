b5bbabe3969e89d529115c65546a9a0ef9cc7334
Ismail Arafa <ismailarafa18@gmail.com>
2018-03-19 18:21:15 +0200

    convert SASS to PostCSS using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const postcssimport = require("postcss-import");
+const vars = require("postcss-simple-vars");
+const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
+const nest = require("postcss-nesting");
+
+gulp.task("ccss", () =>
+  gulp
+    .src("./themes/l0rd-theme/source/css/cc-ss.css")
+    .pipe(postcss([autoprefixer(), postcssimport(), vars()]))
+    .pipe(cssnano())
+    .pipe(rename("cc-ss.min.css"))
+    .pipe(gulp.dest("./themes/l0rd-theme/source/css/"))
+);
+
+gulp.task("custom-css", () =>
+  gulp
+    .src("./themes/l0rd-theme/source/css/style.css")
+    .pipe(postcss([autoprefixer(), postcssimport(), vars(), nest()]))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./themes/l0rd-theme/source/css/"))
+);
+
+gulp.task("styles", ["ccss", "custom-css"]);
